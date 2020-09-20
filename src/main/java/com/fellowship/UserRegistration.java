7de6c0aaa4c66bd439502104b83d25f0b6f8deea
cheluvesha <cheluvesh.b@gmail.com>
2020-09-20 12:35:33 +0530

    [CHELUVESHA]Added : ValidLastName

--- src/main/java/com/fellowship/UserRegistration.java
+++ src/main/java/com/fellowship/UserRegistration.java
@@ -6,14 +6,21 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private Pattern pattern;
     private Matcher matcher;
-    private static final String FIRSTNAME = "^[A-Z]{1}([a-z]{2,15})*";
+    private static final String FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String LASTNAME = "^[A-Z]{1}[a-z]{2,}$";
     public UserRegistration() {
         pattern = pattern.compile(FIRSTNAME);
+        pattern = pattern.compile(LASTNAME);
     }
     public boolean validFirstName(String firstName) {
         matcher = pattern.matcher(firstName);
         return matcher.matches();
 
 
+    }
+
+    public boolean validLastName(String lastName) {
+        matcher = pattern.matcher(lastName);
+        return matcher.matches();
     }
 }
--- src/test/java/com/fellowship/UserRegistrationTest.java
+++ src/test/java/com/fellowship/UserRegistrationTest.java
@@ -7,7 +7,14 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstNameWhenProperReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validFirstName("Cheluvesha");
-        Assert.assertTrue(result);
+        boolean validate = userRegistration.validFirstName("Cheluvesha");
+        Assert.assertTrue(validate);
+    }
+
+    @Test
+    public void givenLastNameWhenProperReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean validate = userRegistration.validLastName("Bharatha");
+        Assert.assertTrue(validate);
     }
 }
