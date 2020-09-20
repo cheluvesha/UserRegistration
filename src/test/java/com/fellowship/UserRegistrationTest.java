74ba36291be04086b1057129b59ff3c52008336e
cheluvesha <cheluvesh.b@gmail.com>
2020-09-20 11:54:00 +0530

    [CHELUVESHA]Added : Welcome message

---
+++ src/test/java/com/fellowship/UserRegistrationTest.java
@@ -0,0 +1,10 @@
+package com.fellowship;
+
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void welcome() {
+        System.out.println("Welcome to USerRegistration");
+    }
+}
